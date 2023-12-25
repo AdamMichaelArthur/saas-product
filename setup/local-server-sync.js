1e3fab0dc19001be4fb0f5446ad68c5bc6d1e161
Adam Arthur <adam.arthur@contentbounty.com>
2023-12-25 12:00:00 +0300

    Afternoon Commit

--- setup/local-server-sync.js
+++ setup/local-server-sync.js
@@ -4,15 +4,18 @@ const { exec } = require('child_process');
 
 require('dotenv').config()
 
-var projectName = "";
+
+var projectName = "saas-product";
+var cwd = path.resolve('.');
+const parentDir = path.dirname(__dirname);
 
 var SERVER="nodemon-server.saas-product.com"
 var REMOTEUSER="root"
 var PATH_REMOTE=`/srv/www/${projectName}/app`
-var PATH_LOCAL=`/Users/adamarthur/OpenProjects/Software/MEAN/${projectName}/app`
+var PATH_LOCAL=`${parentDir}/app`
 
 // Initialize watching on current directory and subdirectories
-let watcher = chokidar.watch(process.env.PATH_LOCAL, { ignored: /(^|[\/\\])\../, persistent: true });
+let watcher = chokidar.watch(PATH_LOCAL, { ignored: /(^|[\/\\])\../, persistent: true });
 
 function getRelativePath(filePath, localPath, remotePath) {
     if (filePath.startsWith(localPath)) {
@@ -30,17 +33,28 @@ watcher
   .on('change', filePath => {
 
     const source = filePath;
-    let relativePath = getRelativePath(filePath, process.env.PATH_LOCAL, process.env.PATH_REMOTE);
-    const destination = `scp ${filePath} ${process.env.REMOTEUSER}@${process.env.SERVER}:${process.env.PATH_REMOTE}/${relativePath}`;
+    let relativePath = getRelativePath(filePath, PATH_LOCAL, PATH_REMOTE);
+    const destination = `scp ${filePath} ${REMOTEUSER}@${SERVER}:${PATH_REMOTE}/${relativePath}`;
     
-    exec(destination, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Execution error: ${error}`);
-            return;
-        }
-    });
-    console.log(destination);
-  })
+    const directoryPath = path.dirname(`${PATH_REMOTE}/${relativePath}`);
+
+    console.log(41, directoryPath);
+
+    let mkDir = `ssh ${REMOTEUSER}@${SERVER} "mkdir -p ${directoryPath}"`
+
+    exec(mkDir, (error, stdout, stderr) => {
+    
+    console.log(44, mkDir);
+        exec(destination, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Execution error: ${error}`);
+                return;
+            }
+        });
+        console.log(destination);
+      })
+    })
+
   .on('unlink', filePath => console.log(`File ${path} has been removed`))
   .on('addDir', filePath => {
 
